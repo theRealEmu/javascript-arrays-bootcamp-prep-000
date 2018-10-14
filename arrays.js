3525c2552a98deee924a3cac4b51ea6fe3f32d4e
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 07:01:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,6 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
 
   array.unshift(element);
-  return;
+  return array;
 
 }
