e1867a7a5a26812e769458b3430cafcc3b00896b
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 07:02:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,6 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
 
   array.unshift(element);
-  return array;
+  return array; //It wouldn't work in one line exept for the use of brackets maybe
 
 }
